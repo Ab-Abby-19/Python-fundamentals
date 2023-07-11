0fc66b59cb2a3b2058cac885bcadef0370afdaee
Abhinav <118174179+Ab-Abby-19@users.noreply.github.com>
2023-07-11 14:14:34 +0530

    Create module_user_defined_functions.py

---
+++ module_user_defined_functions.py
@@ -0,0 +1,18 @@
+# module functions
+
+import math
+print(dir(math))
+from math import log10,sqrt
+print(sqrt(102))
+print(log10(102))
+
+
+# user-defined functions
+
+def sum(first ,second = 5):
+    print("sum: " + str(first + second))
+
+sum(1,7)
+
+
+

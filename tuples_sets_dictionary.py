812d05c6eea405d1defc2f96b7c30b10ef580caa
Abhinav <118174179+Ab-Abby-19@users.noreply.github.com>
2023-07-11 14:13:55 +0530

    Create tuples_sets_dictionary.py

---
+++ tuples_sets_dictionary.py
@@ -0,0 +1,21 @@
+# tuple
+
+marks = (97,97,98,99)
+print(marks.count(96))
+print(marks.index(99))
+print(marks)
+
+# sets
+
+marks = {97,98,99,97,99,98,69,67,69}
+print(marks)
+
+# dictionary
+
+marks = {"maths":99 , "chemistry":90 , "physics": 96}
+print(marks)
+
+marks["physics"]=97
+marks["computer"]=80
+
+print(marks)

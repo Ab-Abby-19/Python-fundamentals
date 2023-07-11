be72e2b596dc838aef57576346459c18db0c0cd2
Abhinav <118174179+Ab-Abby-19@users.noreply.github.com>
2023-07-11 14:12:55 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+# lists
+
+marks = [98,78,99,89,78,67,56,45]
+print(marks)
+marks.append(99)
+print(marks)
+marks.insert(3,69)
+print(marks)
+for i in marks:
+    print (i)
+print(i)
+marks.clear()
+print(marks) 

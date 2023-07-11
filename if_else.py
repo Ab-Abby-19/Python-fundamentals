3c969a5f2e3676e924b80453856c4efea8062b78
Abhinav <118174179+Ab-Abby-19@users.noreply.github.com>
2023-07-11 14:11:35 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+age = input("enter ur age: ")
+age = int(age)
+if age >= 18:
+    print("u r an adult")
+    print("u can vote")
+elif age < 18 and age >= 5:
+    print("u r in school")
+    print("u should study")
+else:
+    print("u r a kid")
+    print("njoy ur life")
+
+print("enjoy ur life at any stage")        
+    
